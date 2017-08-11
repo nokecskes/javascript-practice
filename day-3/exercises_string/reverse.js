a7ed1ce8ed1cf1696f4ac564a72d7e3902c417d6
nokecskes <nokecskes@gmail.com>
2017-08-11 13:43:34 +0200

    add js string exercises

---
+++ day-3/exercises_string/reverse.js
@@ -0,0 +1,8 @@
+'use strict';
+// Reverse the string!
+
+var reversed = ".eslaf eb t'ndluow ecnetnes siht ,dehctiws erew eslaf dna eurt fo sgninaem eht fI";
+
+var readableString = reversed.split("").reverse().join("");
+
+console.log(readableString);
---
+++ day-3/exercises_string/urlfixer.js
@@ -0,0 +1,12 @@
+'use strict';
+// Accidentally I got the wrong URL for a funny subreddit. It's probably "odds" and not "bots"
+// Also, the URL is missing a crutial component, find out what it is and insert it too!
+
+
+var url = "https//www.reddit.com/r/nevertellmethebots";
+
+url = url.replace("bots", "odds");
+url = url.slice(0, 5) + ":" + url.slice(5);
+
+console.log(url);
+
